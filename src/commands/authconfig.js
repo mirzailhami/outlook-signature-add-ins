0bf5da830c666defbd878489d130f6b025bded1b
Mirza Ilhami <mr.ilhami@gmail.com>
2025-05-27 07:42:22 +0700

    Add Graph API to fetch signature from sent email

--- src/commands/authconfig.js
+++ src/commands/authconfig.js
@@ -1,5 +1,4 @@
 export const auth = {
   clientId: "44cb4054-0802-4e2f-8ccb-aba939633fbb",
   authority: "https://login.microsoftonline.com/common",
-  redirectUri: "https://mirzailhami.github.io/outlook-signature-add-ins",
 };
