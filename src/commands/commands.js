b06dcc56589e57edd3033e9ce6b50d1f1ae438e5
Mirza Ilhami <mr.ilhami@gmail.com>
2025-06-10 13:07:21 +0700

    Fix issues on classic outlook

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -922,11 +922,11 @@ function onNewMessageComposeHandler(event) {
 
   const item = Office.context.mailbox.item;
 
-  // displayNotification(
-  //   `Info`,
-  //   `Platform: ${Office.context.mailbox.diagnostics.hostName},
-  //   Version: ${Office.context.mailbox.diagnostics.hostVersion}
-  // );
+  displayNotification(
+    `Info`,
+    `Platform: ${Office.context.mailbox.diagnostics.hostName},
+    Version: ${Office.context.mailbox.diagnostics.hostVersion}`
+  );
   SignatureManager.isReplyOrForward(item, (isReplyOrForward, error) => {
     if (error) {
       logger.log("error", "onNewMessageComposeHandler", { error: error.message });
